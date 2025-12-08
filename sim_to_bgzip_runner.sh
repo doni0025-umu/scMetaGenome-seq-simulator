4f915a85bd048dd35fd1dc046ed5e046d46a6686
Douglas Nilsson <user_douglas@client170-149.wireless.umu.se>
2025-12-08 08:47:27 +0100

    Fixed Cellid formatting: each FASTA now has its own number based on enumeration from the directory where they are sourced from.

--- sim_to_bgzip_runner.sh
+++ sim_to_bgzip_runner.sh
@@ -1,5 +1,2 @@
-cargo run testThur.fna hackThurREMOVEeod.tirp
-bgzip -c testThur.tirp > testThur.tirp.gz
-
-cargo run ncbi_dataset/data/GCF_000006945.2/GCF_000006945.2_ASM694v2_genomic.fna firstout.tirp
-bgzip -c firstout.tirp > firstout.tirp.gz
+cargo run fasta-files/ out.tirp
+bgzip -c out.tirp > out.tirp.gz
--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,11 @@ use rand_distr::{Normal, Distribution};
 use std::io::Write;
 
 
-fn main() {
+fn main() -> std::io::Result<()> {
     // Preamble for parse fasta
     println!("Starting program!");
     let args: Vec<String> = env::args().collect();
-    let file_path: &String = &args[1];
+    let dir_path: &String = &args[1];
 
     // Preamble for mass_shear_seq_amp
     let min_len: usize = 250;
@@ -25,27 +25,32 @@ fn main() {
     let out_path = &args[2];
     let output_file  = std::fs::OpenOptions::new().write(true).truncate(true).create(true).open(out_path).expect("This path could NOT be used as output path. Maybe dir does not exist?");
     let phred_score_prea = (0..150).map(|_| "F").collect::<String>();
-    let base_comp_prea = HashMap::from([('A', 'T'), ('T','A'),('G', 'C'), ('C','G'),]);
-    let fasta_string: String = fs::read_to_string(file_path)
-        .expect("File could not be read - maybe wrong path?");
+    let base_comp_prea = HashMap::from([('A', 'T'), ('T','A'),('G', 'C'), ('C','G'), ('N', 'N'),]);
 
-    // Function calls
-    let fna_to_hashmap  = parse_fasta(fasta_string);
+    for (cellid_hashnum, entry) in fs::read_dir(dir_path)?.enumerate() {
+      let cellid_hashnum = cellid_hashnum + 1;
+      let dir = entry.expect("Could not read dir-entry.");
+      let fasta_string: String = fs::read_to_string(dir.path()).expect("File could not be read - maybe wrong path?");
+    
+      // Function calls
+      let fna_to_hashmap  = parse_fasta(fasta_string);
 
-    mass_seq_shear_amp(fna_to_hashmap, min_len, max_len, fragm_per_bp, frag_len_distr, output_file, &base_comp_prea, &phred_score_prea);
-
+      mass_seq_shear_amp(fna_to_hashmap, min_len, max_len, fragm_per_bp, frag_len_distr, &output_file, &base_comp_prea, &phred_score_prea, cellid_hashnum);
+    }
+ Ok(())
 }
 
-// Taken from Reddit (https://www.reddit.com/r/rust/comments/r5je0y/help_parsing_a_fasta_file/) lol
+
 
 fn parse_fasta(file: String) -> HashMap<String,String> {
+  // Taken from Reddit (https://www.reddit.com/r/rust/comments/r5je0y/help_parsing_a_fasta_file/) lol
     let file_hashmap = file.split(">")         
       .skip(1) // Ignores first empty split - so the empty "" that happens before the first ">"
       .map(|s| {
         let mut lines = s.split("\n");
-        let header = lines.next().unwrap().to_string();
-        let dna = lines.collect::<String>();
-        (header, dna)
+        let contig_name = lines.next().unwrap().to_string();
+        let sequence_str = lines.collect::<String>();
+        (contig_name, sequence_str)
       })
       .collect::<HashMap<String,String>>();
     file_hashmap
@@ -59,9 +64,10 @@ fn mass_seq_shear_amp(hashmap_of_seqs: HashMap<String,String>,
                     max_len: usize, 
                     frag_per_bp: f64, 
                     frag_len_distr: Normal<f32>,
-                    output_file: File, 
+                    output_file: &File, 
                     base_comp: &HashMap<char,char>,
-                    phred_score: &String) -> () {
+                    phred_score: &String,
+                    cellid_hashnum: usize) -> () {
   // Meant to take a (heading, seq) tuple and give the heading coupled with digested fragments (random substrings) stored in a Vectors with string elements.
   // Each sample should have equal amounts of sequencing depth. That SHOULD correspond to the number of times looped over a specific genome(?)
   // -- This means that different compositions of microbiome - i.e 1 E. coli vs 2 S. Typhimurim should be specified in the fasta file used for input.
@@ -71,9 +77,9 @@ fn mass_seq_shear_amp(hashmap_of_seqs: HashMap<String,String>,
   
   let output_file = output_file;
 
-  for (identifier, sequence_str)in hashmap_of_seqs {
+  for (contig_name, sequence_str)in hashmap_of_seqs {
     let seq_len = sequence_str.len();
-    for idx in 1..=((frag_per_bp*(sequence_str.len() as f64)).floor() as i32) {  //125000 would roughly be equal to 50 Megabits of DNA string and 1 000 000 will roughly equal 50 MB of DNA string (in UTF-8 encoding).
+    for _ in 1..=((frag_per_bp*(sequence_str.len() as f64)).floor() as i32) {  //125000 would roughly be equal to 50 Megabits of DNA string and 1 000 000 will roughly equal 50 MB of DNA string (in UTF-8 encoding).
       let start_seq_idx: usize = rand::rng().random_range(0..=seq_len);
       // Loop to ensure fragment is within length limits
       let fragment_len: usize = loop {
@@ -89,10 +95,10 @@ fn mass_seq_shear_amp(hashmap_of_seqs: HashMap<String,String>,
         let mut fragment = String::new();
         fragment.push_str(&sequence_str[start_seq_idx..]);
         fragment.push_str(&sequence_str[..(start_seq_idx+fragment_len-seq_len)]);
-        format_and_write_to_tirp_line((&identifier, &fragment), &output_file, &base_comp, &phred_score, &idx);
+        format_and_write_to_tirp_line((&contig_name, &fragment), &output_file, &base_comp, &phred_score, &cellid_hashnum);
       } else {
         let fragment = sequence_str[start_seq_idx..(&start_seq_idx+&fragment_len)].to_string();
-        format_and_write_to_tirp_line((&identifier, &fragment), &output_file, &base_comp, &phred_score, &idx);
+        format_and_write_to_tirp_line((&contig_name, &fragment), &output_file, &base_comp, &phred_score, &cellid_hashnum);
       }
       // System feedback for every 5000th fragment
 /*       if fragments.len() % 5000 == 0 {
@@ -102,21 +108,21 @@ fn mass_seq_shear_amp(hashmap_of_seqs: HashMap<String,String>,
 }
                    }
 
-fn format_and_write_to_tirp_line(tup_ident_fragment: (&String, &String),
+fn format_and_write_to_tirp_line(tup_contigname_fragment: (&String, &String),
                                 mut output_file: &File, 
                                 base_comp: &HashMap<char,char>, 
                                 phred_score: &String,
-                                idx: &i32) -> () {
+                                cellid_hashnum: &usize,) -> () {
   // Purpose is to use the simulated fragments, reformat them to reads and write to a .tirp file.
   // --Thinking that maybe using the identifier as the cell-id?
   // Init an output file
   // Add to main()
   // Make cell id to be used for column of index 0 in tirp
-  let mut cell_id = tup_ident_fragment.0[..11].to_string();
+  let mut cell_id = tup_contigname_fragment.0[..11].to_string();
   cell_id.push_str("#");
-  cell_id.push_str(&(format!("{:06}", idx)));
+  cell_id.push_str(&(format!("{:06}", cellid_hashnum)));
 
-  let fragment = tup_ident_fragment.1;
+  let fragment = tup_contigname_fragment.1;
   // Colidx 1 to 3 is nonsense added in written string UPDATE: I see it is 1 to 2 now.
   // Make r1 and r2, cols of idx 4 and 5
   let r1 = &fragment[..150];
@@ -125,15 +131,5 @@ fn format_and_write_to_tirp_line(tup_ident_fragment: (&String, &String),
   // q1 and q2 (colidx 6 to 7) will point to phred_score and last col is a blankspace
   let out_str_line = format!("{}\t1\t1\t{}\t{}\t{}\t{}\t \n", cell_id, r1, r2, &phred_score, &phred_score);
   let _ = output_file.write_all(out_str_line.as_bytes()).expect("Problem with writing tirp data content");
-  if idx % 5000 == 0 {
-    println!("{}", out_str_line)
-  }
     
 }
-
-/* Oklart om funktioner är bäst då många scopes kommer uppstå och därmed kan saker försvinna ur heap???? Counter är ju naturligtvis att se till att allt som ska vara i stacken är saker du faktiskt behöver ha med dig.
-
-}
-
-}
-*/
